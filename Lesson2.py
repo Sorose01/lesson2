f9c2bc8511e2e3b60d5458e0c7243206570b3840
Sorose01 <adwords.roman@gmail.com>
2024-06-25 03:02:20 +0300

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,7 @@
+# Задание по работе "Переменные"
+
+task_completed = 12
+working_hours = 1.5
+name_of_the_course = 'Python'
+time_per_task = working_hours / task_completed
+print('Курс:',name_of_the_course,',','всего задач:',task_completed,',','затрачено часов:',working_hours,',','среднее время выполнения',time_per_task,'часа''.')
